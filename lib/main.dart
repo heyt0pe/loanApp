f83d17edba00f1ec59a9653626b4f5311026cb04
Thopeakin <akinthope@gmail.com>
2020-05-14 20:14:11 +0100

    new

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,7 @@ class _HomeState extends State<Home> {
     try {
       infinityPageController.animateToPage(
         index + 1,
-        duration: Duration(milliseconds: 300),
+        duration: Duration(milliseconds: 500),
         curve: Curves.ease,
       );
     } catch (e) {}
